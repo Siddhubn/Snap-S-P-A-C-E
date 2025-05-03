a909a35137196281d2ff4ef9a0092d56b48df82c
Siddharth  B N <143517174+Siddhubn@users.noreply.github.com>
2025-05-03 11:06:37 +0000

    preparing for deploying

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = 'hello123'  # Replace with a strong secret key
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/gallery_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///local.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADS_DEFAULT_DEST = os.path.join(os.getcwd(), 'static', 'uploads')
-    UPLOADED_PHOTOS_DEST = os.path.join(os.getcwd(), 'static', 'uploads')
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    UPLOADS_DEFAULT_DEST = os.path.join(BASE_DIR, 'static', 'uploads')
+    UPLOADED_PHOTOS_DEST = os.path.join(BASE_DIR, 'static', 'uploads')
--- migrations/versions/5a71d79f0bbd_initial_migration.py
+++ migrations/versions/5a71d79f0bbd_initial_migration.py
@@ -1,8 +1,8 @@
-"""empty message
+"""initial migration
 
-Revision ID: 73212ac2239f
+Revision ID: 5a71d79f0bbd
 Revises: 
-Create Date: 2024-12-18 18:41:14.739930
+Create Date: 2025-05-03 11:05:27.363334
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '73212ac2239f'
+revision = '5a71d79f0bbd'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -21,7 +21,7 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=25), nullable=False),
-    sa.Column('password_hash', sa.String(length=128), nullable=True),
+    sa.Column('password_hash', sa.String(length=256), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
